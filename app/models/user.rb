450eac1c4e3c4cc595cb8bdca4d7f2f42f27df2c
Mathieu Audebert <matheosmat@gmail.com>
2017-08-21 19:27:23 +0200

    pages#home v1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,11 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true, allow_blank: false
   validates :encrypted_password, presence: true, uniqueness: true, allow_blank: false, format: { :with => email_regex }
-  validates :firstname, presence: true, allow_blank: false
-  validates :lastname, presence: true, allow_blank: false
-  validates :adress, presence: true, allow_blank: false
-  validates :phonenumber, presence: true, allow_blank: false
-  validates :cleaner, presence: true, allow_blank: false
+  # validates :firstname, presence: true, allow_blank: false
+  # validates :lastname, presence: true, allow_blank: false
+  # validates :adress, presence: true, allow_blank: false
+  # validates :phonenumber, presence: true, allow_blank: false
+  # validates :cleaner, presence: true, allow_blank: false
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
