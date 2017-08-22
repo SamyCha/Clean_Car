680eac7b032157b299b23bf639c8af35ca960df9
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-08-22 14:11:49 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @address = params["address"]
     @users = User.all
     @cleaners = User.where(cleaner: true)
-                    .near(@address, 500)
+                    .near(@address, 20)
     #ici ce n'est pas tous les User que l'on affiche mais seulement les cleaners (à corriger)
     @hash = Gmaps4rails.build_markers(@cleaners) do |cleaner, marker|
       marker.lat cleaner.latitude
