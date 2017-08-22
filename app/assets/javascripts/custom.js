39c6951dab6a6d46f57a7b3ee09da49a0ebdbfb4
Mathieu Audebert <matheosmat@gmail.com>
2017-08-22 18:48:19 +0200

    some fixes, timeout on flashes and add home in dropdown menu

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,6 +2,7 @@ $('.dropdown-toggle').dropdown()
 
 $(document).ready(function(){
   setTimeout(function(){
+    $('#flash').css('-webkit-animation', 'fadeOut 500ms');
     $('#flash').remove();
   }, 4000);
  })
