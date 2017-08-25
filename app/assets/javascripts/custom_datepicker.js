cbacb82cb8e7242b1e96898a061390f3598ea3b2
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-08-25 16:48:14 +0200

    minor changes

--- app/assets/javascripts/custom_datepicker.js
+++ app/assets/javascripts/custom_datepicker.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
     disableMobile: true,
     defaultDate: new Date(),
     dateFormat: "d-m-Y",
+    minDate: "today",
+    // "disable": [
+    //   function(date) {
+    //     // return true to disable
+    //     return (date.getDay() === 6 || date.getDay() === 0);
+    //   }
+    // ],
+   "locale": {
+    "firstDayOfWeek": 1 // start week on Monday
+    }
   });
-  // different syntax $("#date_picker").flatpickr({disableMobile: true});
 });
