7ab49fe9a9219af31dddeb652c9a8da898845d67
Mathieu Audebert <matheosmat@gmail.com>
2017-08-30 18:28:03 +0200

    order#show

--- app/controllers/client/orders_controller.rb
+++ app/controllers/client/orders_controller.rb
@@ -8,6 +8,9 @@ class Client::OrdersController < ApplicationController
 
   def show
     @order = Order.where(state: 'paid').find(params[:id])
+    @cleaning = @order.cleaning
+    @cleaner = @order.cleaning.user
+    @ratings = []
   end
 
 end
