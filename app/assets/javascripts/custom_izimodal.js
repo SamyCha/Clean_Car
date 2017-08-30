e79874e7f9bdc949d91bdf649fe89990a02a67fc
Samy Chalakani <samychalakani@gmail.com>
2017-08-30 16:23:37 +0200

    some stuffs fixed

--- app/assets/javascripts/custom_izimodal.js
+++ app/assets/javascripts/custom_izimodal.js
@@ -1,7 +1,7 @@
 $(document).ready(function(e){
   $("#cleaner-accepted").iziModal({
-    title: 'Order accepted',
-    subtitle: 'A cleaner is coming to clean your car !',
+    title: 'Process to payment',
+    subtitle: 'Cleaning will be booked after payment',
     icon: 'icon-power_settings_new',
     headerColor: '#32B796',
     width: 600,
@@ -16,7 +16,7 @@ $(document).ready(function(e){
     setTimeout(function(){
       $('#cleaner-accepted').iziModal('open');
     }, 4000);
-    $('.ladda-label').text('Cleaner incoming');
+    $('.ladda-label').text('Cleaner Validated');
     var b = $('.btn.ladda-button.progress-demo');
     b.removeClass('btn-primary');
     b.addClass('btn-success').addClass('ladda-button-disabled');
