0961dc8fd1f62a8daf01cafc9c272c80e6c4b6a0
Konstantin BIFERT <konstantin.bifert@gmail.com>
2017-09-01 12:18:26 +0200

    meta image

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -7,9 +7,9 @@ module MetaTagsHelper
     content_for?(:meta_description) ? content_for(:meta_description) : DEFAULT_META["book an eco wash"]
   end
 
-  # def meta_image
-  #   meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["http://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png"])
-  #   # little twist to make it work equally with an asset or a url
-  #   meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
-  # end
+  def meta_image
+    meta_image = (content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META["http://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png"])
+    # little twist to make it work equally with an asset or a url
+    meta_image.starts_with?("http") ? meta_image : image_url(meta_image)
+  end
 end
